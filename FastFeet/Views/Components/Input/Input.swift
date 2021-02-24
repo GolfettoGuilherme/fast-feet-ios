4be5921f83a884803b5453482642d09101070f40
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2021-02-24 02:20:53 -0300

    initial commit

---
+++ FastFeet/Views/Components/Input/Input.swift
@@ -0,0 +1,20 @@
+//
+//  Input.swift
+//  FastFeet
+//
+//  Created by Guilherme Golfetto on 23/02/21.
+//
+
+import UIKit
+
+class Input: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
