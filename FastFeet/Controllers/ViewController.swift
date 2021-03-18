0a416361590f75d1e4622274c4265cd106bc3614
Frederico Bechara De Paola <frederico.paola@neon.com.br>
2021-03-18 12:11:57 -0300

    changes while in call, using scroll view and moving keyboard

--- FastFeet/Controllers/ViewController.swift
+++ FastFeet/Controllers/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     @IBOutlet weak var txtCpf: TextField!
     
     @IBOutlet weak var btnLogar: UIButton!
@@ -23,11 +25,35 @@ class ViewController: UIViewController {
         
         txtCpf.inicializadorDeComponentes(image: "Man", placeholderText: "CPF", type: .text)
         txtSenha.inicializadorDeComponentes(image: "Lock", placeholderText: "Senha", type: .password)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:UIResponder.keyboardWillHideNotification, object: nil)
+
     }
     
     @IBAction func btnLogarClick(_ sender: UIButton) {
+        txtSenha.endEditing(true)
     }
     
+    @objc func keyboardWillShow(notification:NSNotification) {
+        
+        guard let userInfo = notification.userInfo,
+              let keyboardFrameCGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+        else { return }
+        
+        var keyboardFrame: CGRect = keyboardFrameCGRect
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+
+        var contentInset:UIEdgeInsets = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height + 20
+        scrollView.contentInset = contentInset
+    }
+
+    @objc func keyboardWillHide(notification:NSNotification) {
+
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+    }
     
 }
 
